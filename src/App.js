f4ef063ba4bd504cb7f2c9be21d939f633c20472
Émile <85419994+Emile-Massumov@users.noreply.github.com>
2022-03-03 19:54:21 -0500

    added real time temp for city

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
     return `${day} ${date} ${month} ${year}`
     }
     return (
-  <div className="app ">
+<div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
     <main>
       <div className="search-box">
         <input
@@ -44,16 +44,20 @@ function App() {
          onKeyPress={search}
         />
         </div>
+        {(typeof weather.main != "undefined") ? ( 
+          <div>
         <div className="location-box">
-        <div className="location">Montréal CAN</div>
+        <div className="location">{weather.name}, {weather.sys.country}</div>
         <div className="date">{dateBuilder(new Date ())}</div>
          </div>
          <div className="weather-box">
          <div className="temp">
-            20°C
+            {Math.round(weather.main.temp)}°C
            </div>
-           <div className="weather">Sunny</div>
+           <div className="weather">{weather.weather[0].main}</div>
            </div>
+           </div>
+        ) : ('')}
     </main>
     </div>
  
